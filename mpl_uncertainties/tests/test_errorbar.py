ead5624ebb91e4bd14c19e58cf56cbbc8fb95984
zahar <zfrhv2010@gmail.com>
2025-06-16 21:43:05 +0300

    added fit plotting

--- mpl_uncertainties/tests/test_errorbar.py
+++ mpl_uncertainties/tests/test_errorbar.py
@@ -4,7 +4,7 @@
 # Copyright (c) andrewgsavage.
 # Distributed under the terms of the Modified BSD License.
 
-from ..plots import errorbar
+from ..plots import errorbar, fit
 
 
 import numpy as np
@@ -35,5 +35,23 @@ def test_errorbar():
     )
 
     errorbar(x, y)
+    fig = plt.gcf()
+    return fig
+
+@pytest.mark.mpl_image_compare
+def test_fit():
+    x = np.array(
+        [
+            ufloat(1.3, 0.16),
+            ufloat(2.1, 0.15),
+            ufloat(3, 0.22),
+            ufloat(4.2, 0.19),
+            ufloat(4.9, 0.21),
+        ]
+    )
+    slope = ufloat(1, 0.2)
+    intercept = ufloat(0, 0.2)
+
+    fit(x, slope, intercept)
     fig = plt.gcf()
     return fig
